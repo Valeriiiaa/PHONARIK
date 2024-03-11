09552e5149fe00623761f4a083187f53c86cd4c3
Valeriiiaa <113591597+Valeriiiaa@users.noreply.github.com>
2024-03-11 20:38:49 +0200

    added roomDeviceCell

---
+++ LedLamp/Classes/Cells/RoomDeviceCell.swift
@@ -0,0 +1,54 @@
+//
+//  RoomDeviceCell.swift
+//  LedLamp
+//
+//  Created by Valeriya Chernyak on 11.03.2024.
+//
+
+import UIKit
+
+class RoomDeviceCell: UITableViewCell {
+   
+    @IBOutlet weak var hexLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var bckgroundCellView: UIView!
+    @IBOutlet weak var switchView: ConfigurableSwitchControl!
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var imageDevice: UIImageView!
+    
+    var menuButtonDidTap: (() -> Void)?
+    var switchValueChanged: ((Bool) -> Void)?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageDevice.layer.cornerRadius = 16
+        imageDevice.layer.masksToBounds = true
+        bckgroundCellView.layer.cornerRadius = 40
+        bckgroundCellView.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+   
+    @IBAction func menuBtnDIdTap(_ sender: Any) {
+        menuButtonDidTap?()
+    }
+   
+    @IBAction func switcherDidTap(_ sender: Any) {
+        switchValueChanged?(switchView.isOn)
+        stateLabel.text = switchView.isOn ? "connect".localized : "disconnect".localized
+        stateLabel.textColor = switchView.isOn ? .activeText : .inactiveText
+    }
+    
+   func configure(imageDevice: UIImage, stateLabel: Bool, hexLabel: String, instensityLabel: String) {
+        self.imageDevice.image = imageDevice
+        self.stateLabel.text = stateLabel ? "connect".localized : "disconnect".localized
+        self.hexLabel.text = hexLabel
+        intensityLabel.text = instensityLabel
+        DispatchQueue.main.async {
+            self.switchView.setOn(stateLabel, animated: false)
+        }
+    }
+}
--- LedLamp/Classes/ViewControllers/AddLightViewController.swift
+++ LedLamp/Classes/ViewControllers/AddLightViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class AddLightViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var youDontHaveAnyLightLabel: UILabel!
     @IBOutlet weak var lampImage: UIImageView!
     @IBOutlet weak var roomNameLabel: UILabel!
@@ -17,20 +18,106 @@ class AddLightViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         youDontHaveAnyLightLabel.text = "youDontHaveLight".localized
-        roomNameLabel.text = "".localized
-        devicesConnectedLabel.text = "".localized
+        tableView.register(UINib(nibName: "RoomDeviceCell", bundle: nil), forCellReuseIdentifier: "RoomDeviceCell")
         
-       
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    private func configureTableView() {
+        let isEmptyLights = DatabaseManager.shared.load().isEmpty
+        tableView.showsVerticalScrollIndicator = false
+        tableView.contentInset = .init(top: 0, left: 0, bottom: 100, right: 0)
+        tableView.isHidden = isEmptyLights
+        tableView.isHidden = !isEmptyLights
+        youDontHaveAnyLightLabel.isHidden = !isEmptyLights
+    }
+   
     @IBAction func addLightBtnDidTap(_ sender: Any) {
     }
    
     @IBAction func menuBtnDidTap(_ sender: Any) {
     }
     
-
     @IBAction func backBtnDidTap(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    func editName(lightModel: LampModel) {
+        let alertController = UIAlertController(title: "New Room", message: "Please enter a room name", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.text = lightModel.name
+            textField.placeholder = "Room name..."
+            textField.keyboardType = .default
+            textField.autocorrectionType = .no
+        }
+        let okAction = UIAlertAction(title: "Save", style: .default) {
+            action in guard let textField = alertController.textFields?.first,
+                            let text = textField.text else {
+                return
+            }
+            lightModel.name = text
+            DatabaseManager.shared.update(lightModel)
+            ActionManager.shared.reload()
+            
+            guard let accessory = lightModel.accessory else { return }
+            
+            HomeManager.shared.updateName(accessory, name: text, { _ in })
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
+extension AddLightViewController: UITableViewDelegate, UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RoomDeviceCell", for: indexPath)
+        let lightModel = DatabaseManager.shared.load()[indexPath.row]
+        (cell as? RoomDeviceCell)?.configure(imageDevice: UIImage(), stateLabel: lightModel.isEnabled, hexLabel: UIColor(hex: lightModel.color).hexValue(), instensityLabel: <#T##String#>)
+        
+        (cell as? MainScreenCell)?.switchValueChanged = { [unowned self] value in
+            lightModel.isEnabled = value
+            DatabaseManager.shared.update(lightModel)
+            lightModel.accessory?.getCharacteristic(forType: .power)?.writeValue(lightModel.isEnabled, completionHandler: { error in
+                print(error)
+            })
+        }
+        (cell as? RoomDeviceCell)?.menuButtonDidTap = { [unowned self] in
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "editName".localized, style: .default, handler: { [unowned self] _ in
+                self.editName(lightModel: lightModel)
+            }))
+            alert.addAction(UIAlertAction(title: "delete".localized, style: .destructive, handler: { [unowned tableView] _ in
+                let alert = UIAlertController(title: nil, message: "wantToDelete".localized, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "yes".localized, style: .default, handler: { _ in
+                    DispatchQueue.main.async {
+                        DatabaseManager.shared.remove(lightModel.name)
+                        ActionManager.shared.reload()
+                        guard let accessory = lightModel.accessory else { return }
+                        HomeManager.shared.removeDevice(accessory, { _ in })
+                    }
+                }))
+                alert.addAction(UIAlertAction(title: "no".localized, style: .cancel))
+                self.present(alert, animated: true)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "cancel".localized, style: .cancel))
+            alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+            present(alert, animated: true)
+        }
+        return cell
     }
     
 }
